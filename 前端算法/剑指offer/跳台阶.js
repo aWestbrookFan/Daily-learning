6740436ea47446c54bf5ae5c91f2f2917397e92b
B17041923沈文武 <2587542631@qq.com>
2021-03-29 16:24:01 +0800

    新版本

---
+++ 前端算法/剑指offer/跳台阶.js
@@ -0,0 +1,19 @@
+/**
+@desc:
+@author:sww
+@date 2021-03-24
+@time 20:46
+
+
+*/
+
+/*
+*
+* 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+* */
+
+var numWays = function(n) {
+
+};
+
+console.log(numWays());
---
+++ 前端算法/剑指offer/顺时针打印矩阵.js
@@ -0,0 +1,20 @@
+/**
+@desc:
+@author:sww
+@date 2021-03-24
+@time 20:41
+
+*/
+/*
+*
+* 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+* */
+var matrix = [[1,2,3],[4,5,6],[7,8,9]];
+
+var spiralOrder = function(matrix) {
+
+};
+
+spiralOrder()
+
+
